c7a91430a3278f5c8f1c597ed3c03ff9606161b5
Yaswanth <chyaswanth2001@gmail.com>
2025-03-16 23:56:02 +0530

    intial commit of the project

---
+++ src/main/java/com/yaswanth/lpms/LpmsApplication.java
@@ -0,0 +1,13 @@
+package com.yaswanth.lpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpmsApplication.class, args);
+	}
+
+}
